ef673cd05da7385debec800bda85c969539b2e50
kaverighawate <ghawatekaveri@gmail.com>
2025-02-12 22:53:08 +0530

    Add files via upload

---
+++ p1.js
@@ -0,0 +1,17 @@
+
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    
+    if (name && email && message) {
+        alert('Thank you for your message! I will get back to you shortly.');
+       
+        document.getElementById('contact-form').reset();
+    } else {
+        alert('Please fill in all fields.');
+    }
+});
